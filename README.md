b351f936bba9625432c7651a12f8b11676b81632
xavier@fedora32HPEnnvy <xavier@fedora32HPEnnvy>
2020-08-29 19:05:25 +0200

    fixed bug on algorithm

--- README.md
+++ README.md
@@ -6,7 +6,9 @@ Various pixel by pixel image construction utilities
 
 Autostrereogram(ASG) are images that can be viewed as stereo image (look it up in Wikipedia) without resorting to any external device.
 
-ASG are computed using both a Pattern and a Depth-Map as input. Utilities are provided to generate these from standanrd png images.
+ASG are computed using both a Pattern and a Depth-Map as input. Utilities are provided to generate these from standard png images.
 
 See autostereogram_test for examples.
+
+
 
--- autostereogram.go
+++ autostereogram.go
@@ -9,18 +9,27 @@ func NewASG(pat *Pattern, dmap *image.Gray) image.Image {
 	l, L := 200, 200
 
 	rgba := image.NewNRGBA(dmap.Bounds())
+
 	for y := 0; y < rgba.Rect.Dy(); y++ {
+		// y is vertical coordiante. No influence on stereo effect.
+
 		for x := 0; x < rgba.Rect.Dx(); x++ {
-			// fill with pattern
+			// first, fill result image with pattern
 			rgba.Set(x, y, pat.At(x, y))
 		}
+
 		for xz := 0; xz < rgba.Rect.Dx(); xz++ {
-			// force right eye value to match left eye value
+			// xz is the depth map horizontal coordinate, z is the gray color, representing the depth.
+			// darker is closer, white is far away.
 			z := int(dmap.GrayAt(xz, y).Y)
-			dx := (z * L) / (z + l)
-			x := xz - dx/2
+			// when both eyes point to xz, the difference between the points where the rays intersect the image is 2*dx
+			dx := (z * L) / ((z + l) * 2)
+			// this x-dx is the left) eye intersect
+			x := xz - dx
 			if x > 0 {
+				// get pixel for the left eye ...
 				c := rgba.At(x, y)
+				// ... copy in the right eye pixel intersect, x+dx
 				rgba.Set(x+dx, y, c)
 			}
 		}
